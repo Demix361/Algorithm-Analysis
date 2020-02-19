6ba205a570950bf67cb012801d15864c5e751be9
Demix361 <49446810+Demix361@users.noreply.github.com>
2020-02-19 19:21:28 +0300

    )

--- lab_4_backup/code/controller.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include <string>
-#include <memory>
-#include <vector>
-
-void enter_word(std::string&, std::string);
-void controller();
--- lab_4_backup/old/asdf.py
+++
@@ -1,6 +0,0 @@
-
-
-for i in range(0, 4):
-    print(i)
-for i in range(4, 8):
-    print(i)
