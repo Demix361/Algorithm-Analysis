9fa55eddf31da93e4644fbe828503b715699b6de
Demix361 <ti-aminov@yandex.ru>
2020-02-24 17:40:58 +0300

    lab 5-7

--- lab_6/egor_code/matrix.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include<vector>
-
-#define N_MIN	3			// минимальное количество вершин
-#define N_MAX	30			// максимальное количество вершин
-
-using Matrix = std::vector<std::vector<double>>;
--- lab_6/egor_code/total_search.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <vector>
-#include "matrix.h"
-
-double total_search(Matrix matrix, std::vector<int> &way);
